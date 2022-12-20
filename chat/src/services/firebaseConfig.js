b1b3b4adfc0478536974fb2997c6920f4b2ef116
maycon-santana <maycon.santana@thstec.com.br>
2022-12-20 19:51:25 -0300

    Configuração Firebase

---
+++ chat/src/services/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBblszYv_ElcgkIIewDKocvKDpYAs7JaPk",
+  authDomain: "chat-test-76e21.firebaseapp.com",
+  projectId: "chat-test-76e21",
+  storageBucket: "chat-test-76e21.appspot.com",
+  messagingSenderId: "976364720589",
+  appId: "1:976364720589:web:b2b195a3c858c518b9b484",
+  measurementId: "G-MHSNR23VNH"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const databaseApp = getFirestore(app);
